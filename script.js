dd4537f0d55ac863e4669bd094524b34395f19f2
EricaMarzi <erica.marzi.1993@gmail.com>
2023-12-18 14:46:23 +0100

    Scaletta

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("JS OK")
+
+
+/* 
+- Creare in pagina l'elemento
+- Chiedere all'utente il numero di km
+- Chiedere all'utente l'età del passeggero
+- Calcolare il totale prezzo del viaggio 
+- Impostare massimo due decimali dopo la virgola
+- Richiamare l'elemento con il risultato
+*/
+
